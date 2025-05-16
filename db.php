92e81446f775b5964efe327fceaa7b3bb762a8fe
wiriya <wiriyaphlsa@gmail.com>
2025-05-16 15:00:31 +0700

    add .gitignore and db.php change

--- db.php
+++ db.php
@@ -9,4 +9,13 @@ $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$dotenv = parse_ini_file('.env');
+$conn = new mysqli(
+    $dotenv['DB_HOST'],
+    $dotenv['DB_USER'],
+    $dotenv['DB_PASS'],
+    $dotenv['DB_NAME']
+);
+
 ?>
